8f82b0a7a55d6565e07d6feb7abbbec42a1d4ab2
mnicokar <nicokar.mobin@gmail.com>
2023-04-10 12:08:34 -0700

    aligned icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,16 @@ const Navbar = () => {
       <Container maxWidth="xl">
         <Toolbar>
           <Typography variant="h4" component="div">
-            Mobin Financial <a style = {{textDecoration:'none', color:"inherit" }} href = "https://github.com/mnicokar/mortgageCalculator" > <DiGithubBadge /></a>
+            Mobin Financial{" "}
+            <a
+              style={{ textDecoration: "none", color: "inherit" }}
+              href="https://github.com/mnicokar/mortgageCalculator"
+            >
+              {" "}
+              <DiGithubBadge
+                style={{ position: 'relative' , top: "3px"}}
+              />
+            </a>
           </Typography>
         </Toolbar>
       </Container>
