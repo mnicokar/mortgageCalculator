cfc23c8e7a6f72b1f2445d79e4a789f6d15b0d23
mnicokar <nicokar.mobin@gmail.com>
2023-04-09 21:52:58 -0700

    Added Github Link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+import { DiGithubBadge } from "react-icons/di";
 
 const Navbar = () => {
   return (
     <AppBar position="static">
-        <Container maxWidth = "xl">
-            <Toolbar>
-            <Typography variant="h5" component="div" >
-                Mobin Bank
-            </Typography>
-            </Toolbar>
-        </Container>
+      <Container maxWidth="xl">
+        <Toolbar>
+          <Typography variant="h4" component="div">
+            Mobin Financial <a style = {{textDecoration:'none', color:"inherit" }} href = "https://github.com/mnicokar/mortgageCalculator" > <DiGithubBadge /></a>
+          </Typography>
+        </Toolbar>
+      </Container>
     </AppBar>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
